92138ca461e73ffd529b6e61420a9e9f8dd0333c
NDHuynhDN <duchuynh3011@gmail.com>
2023-06-15 18:24:54 +0700

    update4

--- main.js
+++ main.js
@@ -28,3 +28,17 @@ window.onscroll = () => {
   menuIcon.classList.remove("bx-x");
   navbar.classList.remove("active");
 };
+ScrollReveal({reset:true});
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: "top" });
+ScrollReveal().reveal('.home-img, .services-container, .project-box, .contact form', { origin: "bottom" });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: "left" });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: "right" });
+
